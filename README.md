f9bdd8bb3df0212a8c807b06f182e32375d0f927
Artem Kuznetsov <arkuznetsov@gmail.com>
2022-05-17 16:07:32 +0300

    Первая версия

---
+++ README.md
@@ -0,0 +1,62 @@
+# Oscript SSH client 
+
+[![GitHub release](https://img.shields.io/github/release/ArKuznetsov/oscript-deflate.svg?style=flat-square)](https://github.com/ArKuznetsov/oscript-deflate/releases)
+[![GitHub license](https://img.shields.io/github/license/ArKuznetsov/oscript-deflate.svg?style=flat-square)](https://github.com/ArKuznetsov/oscript-deflate/blob/develop/LICENSE)
+[![GitHub Releases](https://img.shields.io/github/downloads/ArKuznetsov/oscript-deflate/latest/total?style=flat-square)](https://github.com/ArKuznetsov/oscript-deflate/releases)
+[![GitHub All Releases](https://img.shields.io/github/downloads/ArKuznetsov/oscript-deflate/total?style=flat-square)](https://github.com/ArKuznetsov/oscript-deflate/releases)
+
+[![Build Status](https://img.shields.io/github/workflow/status/ArKuznetsov/oscript-deflate/%D0%9A%D0%BE%D0%BD%D1%82%D1%80%D0%BE%D0%BB%D1%8C%20%D0%BA%D0%B0%D1%87%D0%B5%D1%81%D1%82%D0%B2%D0%B0)](https://github.com/arkuznetsov/oscript-deflate/actions/)
+[![Quality Gate](https://open.checkbsl.org/api/project_badges/measure?project=oscript-deflate&metric=alert_status)](https://open.checkbsl.org/dashboard/index/oscript-deflate)
+[![Coverage](https://open.checkbsl.org/api/project_badges/measure?project=oscript-deflate&metric=coverage)](https://open.checkbsl.org/dashboard/index/oscript-deflate)
+[![Tech debt](https://open.checkbsl.org/api/project_badges/measure?project=oscript-deflate&metric=sqale_index)](https://open.checkbsl.org/dashboard/index/oscript-deflate)
+
+## Архиватор Deflate клиент для oscript
+
+## Примеры использования
+
+### Сжатие потока
+
+```bsl
+#Использовать deflateArchiver
+
+ВходящийПоток = Новый ФайловыйПоток("d:\tmp\inputFile.txt");
+СжатыйПоток = Новый ПотокВПамяти();
+
+Архиватор = Новый АрхиваторDeflate();
+Архиватор.УпаковатьПоток(ВходящийПоток, СжатыйПоток, 1);
+
+```
+
+### Распаковка потока
+
+```bsl
+#Использовать deflateArchiver
+
+СжатыйПоток = Новый ПотокВПамяти();
+ИсходящийПоток = Новый ФайловыйПоток("d:\tmp\outputFile.txt");
+
+Архиватор = Новый АрхиваторDeflate();
+Архиватор.РаспаковатьПоток(СжатыйПоток, ИсходящийПоток);
+
+```
+
+### Сжатие файла
+
+```bsl
+#Использовать deflateArchiver
+ 
+Архиватор = Новый АрхиваторDeflate();
+Архиватор.УпаковатьФайл("d:\tmp\inputFile.txt", "d:\tmp\outputFile.dfl");
+
+```
+
+### Распаковка файла
+
+```bsl
+#Использовать deflateArchiver
+
+Архиватор = Новый АрхиваторDeflate();
+Архиватор.РаспаковатьФайл("d:\tmp\inputFile.dfl", "d:\tmp\outputFile.txt");
+
+```
+
---
+++ src/oscript-component/DeflateArchiver.cs
@@ -0,0 +1,163 @@
+﻿/*----------------------------------------------------------
+Use of this source code is governed by an MIT-style
+license that can be found in the LICENSE file or at
+https://opensource.org/licenses/MIT.
+----------------------------------------------------------
+// Codebase: https://github.com/ArKuznetsov/oscript-deflate/
+----------------------------------------------------------*/
+
+using System.IO;
+using System.IO.Compression;
+using ScriptEngine.Machine;
+using ScriptEngine.Machine.Contexts;
+using ScriptEngine.HostedScript.Library.Binary;
+
+namespace oscriptcomponent
+{
+    /// <summary>
+    /// АрхиваторDeflate
+    /// </summary>
+    [ContextClass("АрхиваторDeflate", "DeflateArchiver")]
+    public class DeflateArchiver : AutoContext<DeflateArchiver>
+    {
+
+        /// <summary>
+        /// Упаковать поток
+        /// </summary>
+        [ContextMethod("УпаковатьПоток")]
+        public void CompressStream(IValue InputStream, IValue OutputStream, int OutputCompressionLevel = 2)
+        {
+
+            CompressionLevel streamCompressionLevel;
+
+            switch (OutputCompressionLevel)
+            {
+                case 0:
+                    streamCompressionLevel = CompressionLevel.NoCompression;
+                    break;
+                case 1:
+                    streamCompressionLevel = CompressionLevel.Fastest;
+                    break;
+                case 2:
+                    streamCompressionLevel = CompressionLevel.Optimal;
+                    break;
+                default:
+                    streamCompressionLevel = CompressionLevel.Optimal;
+                    break;
+            }
+
+            DeflateStream compressor;
+
+            if (OutputStream.AsObject() is IStreamWrapper outputStreamWrapper)
+            {
+                compressor = new DeflateStream(outputStreamWrapper.GetUnderlyingStream(), streamCompressionLevel, true);
+            }
+            else
+            {
+                throw RuntimeException.InvalidArgumentType("OutputStream");
+            }
+
+            if (InputStream.AsObject() is IStreamWrapper inputStreamWrapper)
+            {
+                inputStreamWrapper.GetUnderlyingStream().CopyTo(compressor);
+                compressor.Close();
+            }
+            else
+            {
+                throw RuntimeException.InvalidArgumentType("InputStream");
+            }
+
+        }
+
+        /// <summary>
+        /// Распаковать поток
+        /// </summary>
+        [ContextMethod("РаспаковатьПоток")]
+        public void DecompressStream(IValue InputStream, IValue OutputStream)
+        {
+
+            DeflateStream decompressor;
+
+            if (InputStream.AsObject() is IStreamWrapper inputStreamWrapper)
+            {
+                decompressor = new DeflateStream(inputStreamWrapper.GetUnderlyingStream(), CompressionMode.Decompress, true);
+            }
+            else
+            {
+                throw RuntimeException.InvalidArgumentType("InputStream");
+            }
+
+            if (OutputStream.AsObject() is IStreamWrapper outputStreamWrapper)
+            {
+                decompressor.CopyTo(outputStreamWrapper.GetUnderlyingStream());
+                decompressor.Close();
+            }
+            else
+            {
+                throw RuntimeException.InvalidArgumentType("OutputStream");
+            }
+
+        }
+
+        /// <summary>
+        /// Упаковать файл
+        /// </summary>
+        [ContextMethod("УпаковатьФайл")]
+        public void CompressFile(IValue InputFileName, IValue OutputFileName, int OutputCompressionLevel = 2)
+        {
+
+            CompressionLevel fileCompressionLevel;
+
+            switch (OutputCompressionLevel)
+            {
+                case 0:
+                    fileCompressionLevel = CompressionLevel.NoCompression;
+                    break;
+                case 1:
+                    fileCompressionLevel = CompressionLevel.Fastest;
+                    break;
+                case 2:
+                    fileCompressionLevel = CompressionLevel.Optimal;
+                    break;
+                default:
+                    fileCompressionLevel = CompressionLevel.Optimal;
+                    break;
+            }
+
+            FileStream inputFileStream = File.Open(InputFileName.AsString(), FileMode.Open);
+            FileStream outputFileStream = File.Create(OutputFileName.AsString());
+            var compressor = new DeflateStream(outputFileStream, fileCompressionLevel);
+            inputFileStream.CopyTo(compressor);
+            compressor.Close();
+            inputFileStream.Close();
+            outputFileStream.Close();
+        }
+
+        /// <summary>
+        /// Распаковать файл
+        /// </summary>
+        [ContextMethod("РаспаковатьФайл")]
+        public void DecompressFile(IValue InputFileName, IValue OutputFileName)
+        {
+
+            FileStream inputFileStream = File.Open(InputFileName.AsString(), FileMode.Open);
+            FileStream outputFileStream = File.Create(OutputFileName.AsString());
+            var decompressor = new DeflateStream(inputFileStream, CompressionMode.Decompress);
+            decompressor.CopyTo(outputFileStream);
+            decompressor.Close();
+            inputFileStream.Close();
+            outputFileStream.Close();
+        }
+
+        /// <summary>
+        /// Создает АрхиваторDeflate
+        /// </summary>
+        /// <returns>АрхиваторDeflate</returns>
+        [ScriptConstructor]
+        public static IRuntimeContextInstance Constructor()
+        {
+            return new DeflateArchiver();
+        }
+
+    }
+}
